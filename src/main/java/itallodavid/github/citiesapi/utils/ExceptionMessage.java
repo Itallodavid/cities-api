14841b5cf0f12ab4e922e470c562618086f29901
Itallodavid <itallodavid.fdasilva@gmail.com>
2021-07-27 22:27:35 -0300

    Add utility class for error messages

---
+++ src/main/java/itallodavid/github/citiesapi/utils/ExceptionMessage.java
@@ -0,0 +1,6 @@
+package itallodavid.github.citiesapi.utils;
+
+public final class ExceptionMessage {
+    public static String ENTITY_NOT_FOUND = "The entity was not found in the database.";
+    private ExceptionMessage() {}
+}
