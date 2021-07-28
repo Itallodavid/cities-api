bd00e20484442b24fc647f27a68ab7a7b3faf28f
Itallodavid <itallodavid.fdasilva@gmail.com>
2021-07-27 22:25:01 -0300

    Add new method to retrieve an entity

--- src/main/java/itallodavid/github/citiesapi/services/CountryService.java
+++ src/main/java/itallodavid/github/citiesapi/services/CountryService.java
@@ -1,5 +1,6 @@
 package itallodavid.github.citiesapi.services;
 
+import itallodavid.github.citiesapi.exceptions.EntityNotFoundException;
 import itallodavid.github.citiesapi.models.Country;
 import itallodavid.github.citiesapi.repositories.CountryRepository;
 import lombok.AllArgsConstructor;
@@ -8,13 +9,25 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
 @Service
+@Transactional(readOnly = true)
 @AllArgsConstructor
 public class CountryService {
     private final CountryRepository repository;
 
-    @Transactional(readOnly = true)
     public Page<Country> getCountries(final Pageable pageable) {
         return repository.findAll(pageable);
+    }
+
+    public Country getCountry(final Long id) throws EntityNotFoundException {
+        Optional<Country> country = repository.findById(id);
+
+        if (country.isEmpty()) {
+            throw new EntityNotFoundException();
+        }
+
+        return country.get();
     }
 }
