2e8020bd85d48bcbaf9e26503eb58c51d523db8f
Itallodavid <itallodavid.fdasilva@gmail.com>
2021-08-06 20:34:48 -0300

    Add json type to entity state

--- src/main/java/itallodavid/github/citiesapi/models/State.java
+++ src/main/java/itallodavid/github/citiesapi/models/State.java
@@ -1,12 +1,17 @@
 package itallodavid.github.citiesapi.models;
 
+import com.vladmihalcea.hibernate.type.json.JsonType;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.Type;
+import org.hibernate.annotations.TypeDef;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity @Table(name = "estado")
 @Getter @Setter
+@TypeDef(name = "json", typeClass = JsonType.class)
 public class State {
     @Id
     private Long id;
@@ -23,5 +28,8 @@ public class State {
     @JoinColumn(name = "pais", referencedColumnName = "id")
     private Country country;
 
-    private String ddd;
+    @Type(type = "json")
+    @Basic(fetch = FetchType.LAZY)
+    @Column(name = "ddd", columnDefinition = "json")
+    private List<Integer> ddd;
 }
